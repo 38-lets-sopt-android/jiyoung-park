ef560562ece368db7144070823b37e05caf7525c
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-04-07 00:34:08 +0900

    [feat] 로그인 화면 구현

--- app/src/main/java/com/example/letssopt/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/MainActivity.kt
@@ -1,17 +1,45 @@
 package com.example.letssopt
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.letssopt.ui.theme.LETSSOPTTheme
+import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +60,138 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("") }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(horizontal = 20.dp)
+    ) {
+        Spacer(modifier = Modifier.height(60.dp))
+
+        Text(
+            text = "watcha",
+            modifier = modifier
+                .fillMaxWidth(),
+            color = Color(0xFFE8003C),
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Column(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "이메일로 로그인",
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+
+                )
+        }
+
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "이메일",
+                modifier = Modifier.fillMaxWidth(),
+                color = Color(0xFF999999),
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Medium,
+            )
+            TextField(
+                value = text,
+                onValueChange = {text = it},
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {Text("이메일 주소를 입력하세요")},
+                shape = RoundedCornerShape(size = 8.dp),
+                colors = TextFieldDefaults.colors(
+                    unfocusedTextColor = Color(0xFF666666),
+                    unfocusedContainerColor = Color(0xFF2A2A2A),
+                    focusedContainerColor = Color(0xFF2A2A2A),
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(18.dp))
+
+        Column (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(
+                text = "비밀번호",
+                modifier = modifier.fillMaxWidth(),
+                color = Color(0xFF999999),
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Medium
+            )
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {Text("비밀번호를 입력하세요")},
+                shape = RoundedCornerShape(size = 8.dp),
+                colors = TextFieldDefaults.colors(
+                    unfocusedTextColor = Color(0xFF666666),
+                    unfocusedContainerColor = Color(0xFF2A2A2A),
+                    focusedContainerColor = Color(0xFF2A2A2A),
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(334.dp))
+
+        Text(
+            text = "아직 계정이 없으신가요?  회원가입",
+            modifier = modifier
+                .fillMaxWidth()
+                .clickable() {
+                    //val intent = Intent(context,NextActivity::class.java)
+                    //context.startActivity(intent)
+                },
+            color = Color(0xFF999999),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+            shape = RoundedCornerShape(size = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFE8003C)
+            )
+        ) {
+            Text(
+                text = "로그인",
+                color = Color(0xFFFFFFFF),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
