16cf82862c72f30c666d5e26acb818ef78165870
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-08 14:27:37 +0900

    feat/#8: 로그인, 회원가입 API dto 구현

---
+++ app/src/main/java/com/example/letssopt/data/remote/dto/response/LoginResponseDto.kt
@@ -0,0 +1,26 @@
+package com.example.letssopt.data.remote.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponseDto (
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("code")
+    val code: String,
+    @SerialName("data")
+    val data: Int,
+    @SerialName("meta")
+    val meta: UserData? = null,
+)
+
+@Serializable
+data class UserData(
+    @SerialName("userId")
+    val userId: Int,
+)
